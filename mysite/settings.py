af65ea4813ac95aace7ef16429d967ff875aaf20
Pratyush Maharjan <pratyushmaharjan90@gmail.com>
2025-12-31 13:50:01 +0545

    Made some changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wh&1mkw@uo0z^_lhk)i^n057f-@nlmt)h-1rsrdmj4lnq6@hhe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://pratyushhhd.pythonanywhere.com/']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://yourusername.pythonanywhere.com',
+]
+
 
 
 # Application definition
